d57499fcf92a40d4d510a63965313f9454630388
rishabhs-optimus <rishabh.sethi@optimusinfo.com>
2017-10-09 09:51:07 +0530

    added js files

---
+++ index.js
@@ -0,0 +1,10 @@
+/**
+ * Functions to toggle the nav
+ */
+function openRightMenu() {
+    document.getElementById("rightMenu").style.display = "block";
+}
+
+function closeRightMenu() {
+    document.getElementById("rightMenu").style.display = "none";
+}
---
+++ toggle.js
@@ -0,0 +1,9 @@
+
+/**
+ * Hover effect on blog link
+ */
+$(document).ready(function () {
+    $(".c-blog").hover(function () {
+        $(".c-blog-content").toggle();
+    })
+})
